73cae845adf19d9a58e214292e69254fa4bd4762
Stefan <steffheili846@gmail.com>
2021-11-10 15:27:02 +0100

    nach Erklärung

--- Testat3/friends.js
+++ Testat3/friends.js
@@ -1,27 +1,20 @@
-// Collection ID: 870f0156-7cdc-4ce8-885d-b4f05ee6a49e //
-
-// Codevorgaben //
-
-// Macht erstmal keinen Sinn //
-
-window.chatToken = "...";
-window.chatCollectionId = "870f0156-7cdc-4ce8-885d-b4f05ee6a49e";
-window.chatServer = "https://online-lectures-cs.thi.de/chat";
+// Collection ID: 870f0156-7cdc-4ce8-885d-b4f05ee6a49e
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
-if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    let data = JSON.parse(xmlhttp.responseText);
-    console.log(data);
-}
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        let data = JSON.parse(xmlhttp.responseText);
+        console.log(data);
+    }
 };
 
-// Chat Server URL und Collection ID als Teil der URL
-xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId +
-"/user", true);
-// Das Token zur Authentifizierung, wenn notwendig
-xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken);
+// Collection id und Bearer muss angepasst werden 
+xmlhttp.open("GET", "https://online-lectures-cs.thi.de/chat/870f0156-7cdc-4ce8-885d-b4f05ee6a49e/user", true);
+// Add token, e. g., from Tom
+xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NTM4MDQyfQ.qg4H3u18X2wuExSWQ_U1DkmVZnqswj9Z4plNbn5GXlg');
 xmlhttp.send();
 
-
-// ... //
+// Laden aller Nuzernamen 
+addfriend.addEventListener('keyup', function() {
+    consolge.log(addfriend.value);
+});
