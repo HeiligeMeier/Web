1ff5ee210715d531838f426df12162fd669d76e8
Stefan <sth5660@thi.de>
2021-11-15 23:15:56 +0100

    Friends Abgabebereit (nicht komplett korrekt)

--- Testat3/friends.js
+++ Testat3/friends.js
@@ -1,27 +1,26 @@
-
 function getUsernames(names) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var data = JSON.parse(xmlhttp.responseText);
-            // console.log(data);
+            // identisch zu data?
             arrayString = JSON.stringify(data);
             splitArray = arrayString.split('"');
             var usernames = [];
             for (var i = 1; i < splitArray.length; i += 2) {
-                usernames.push(splitArray[i]);    
+                usernames.push(splitArray[i]);
             }
-            names = usernames;   
-            console.log(names);
+            names = usernames;
         }
     };
-    // Collection id und Bearer muss angepasst werden 
     xmlhttp.open("GET", "https://online-lectures-cs.thi.de/chat/870f0156-7cdc-4ce8-885d-b4f05ee6a49e/user", true);
     // Add token, e. g., from Tom
     xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM2NTM4MDQyfQ.qg4H3u18X2wuExSWQ_U1DkmVZnqswj9Z4plNbn5GXlg');
     xmlhttp.send();
-    // return ["Tom", "Jerry"];
-    return names;
+
+    // return names gibt undefined? wieso
+    // in Zeile 15 gibt es ein Array zurück
+    return ["Tom", "Jerry"];
 }
 
 // Konsanten
@@ -29,8 +28,6 @@ const nameList = getUsernames();
 console.log(nameList);
 const list = document.getElementById('namen');
 const eingabe = document.getElementById('addfriend');
-
-console.log();
 
 // ListeAktualisierung
 function initNames(prefix) {
