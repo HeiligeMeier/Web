975f0fcda1b76729326982983b92717d796a9465
Stefan <sth5660@thi.de>
2021-11-17 11:54:45 +0100

    Einfach nur Chaos

--- Testat3/friends.js
+++ Testat3/friends.js
@@ -1,43 +1,64 @@
 window.chatCollectionId = '245e5db0-d33f-41e8-8aef-33e5d1caf9d1';
         window.chatServer = "https://online-lectures-cs.thi.de/chat";
         window.chatToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MDkxNzY4fQ.s68jjWZCECuv9MxYqHDinIPxv10jmNtb_h1ZYh78U_A';
-
-function getUsernames(callback) {
-    var usernames = [];
+/*     
+function getUsernames(names) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function readyState() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            // callback(xmlhttp.responseText);
-            let data = JSON.parse(xmlhttp.responseText);
-            usernames = data;
-            console.log(usernames);
-            /*
-            arrayString = JSON.stringify(data);
-            splitArray = arrayString.split('"');
-            for (var i = 1; i < splitArray.length; i += 2) {
-                usernames.push(splitArray[i]);
-            }
-            */
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+        var usernames = [];
+        let data = JSON.parse(xmlhttp.responseText);
+        names = data;
+        arrayString = JSON.stringify(data);
+        splitArray = arrayString.split('"');
+        for (var i = 1; i < splitArray.length; i += 2) {
+            usernames.push(splitArray[i]);
         }
-    };
+        console.log(usernames);
+        return usernames;
+    }
+};
     xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId + "/user", true);
     // Add token, e. g., from Tom
     xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken);
     xmlhttp.send();
+    alert("Warten");
     return ["Tom", "Jerry"];
 }
-
-/*
-getUsernames((text) => {
-    let data = JSON.parse(text);
-    console.log(data);
-    document.getElementById('namen').innerHTML = text;
-});
 */
+function getUsernames() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function readyState() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            // callback(xmlhttp.responseText);
+        }
+    };         
+    xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId + "/user", true);
+    // Add token, e. g., from Tom
+    xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken);
+    xmlhttp.send();
+    return ["Tom", "Jerry"]; 
+}
+
+const list = document.getElementById('namen');
+var nameList = getUsernames();
+getUsernames((text) => {
+    var usernames = [];
+    let data = JSON.parse(text);
+    arrayString = JSON.stringify(data);
+    splitArray = arrayString.split('"');
+    for (var i = 1; i < splitArray.length; i += 2) {
+        usernames.push(splitArray[i]);
+    }
+    nameList = data;
+    console.log(nameList);
+
+});
+
+console.log(nameList);
 
 // Deklaration
-const nameList = getUsernames();
-const list = document.getElementById('namen');
 const eingabe = document.getElementById('addfriend');
 
 // ListeAktualisierung
