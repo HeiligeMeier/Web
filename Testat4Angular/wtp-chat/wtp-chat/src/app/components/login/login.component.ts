18269917215d9ab41f0b66a42b2d535b1ae4cc4d
Stefan <steffheili846@gmail.com>
2021-11-30 16:58:15 +0100

    habs auskommentiert, damits ausführbar ist

--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/login/login.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/login/login.component.ts
@@ -6,11 +6,37 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    public username: string = '';
+    public password: string = '';
+    public usernamevalid: boolean = false;
+    public passwordvalid: boolean = false;
 
-    public constructor() { 
+    public constructor() {
     }
 
     public ngOnInit(): void {
     }
+    // accounts ist ein Array für registrierte Nutzer
+    /*
+    public usernamecheck(): void {
+        for (let i = 0; i < accounts.length(); i++)
+        if (usernamelogin.value == accounts[i]) {
+            this.usernamevalid = true;
+        }
+    }
 
+    public passwordcheck(): void {
+        if () {
+            this.passwordvalid = true;
+        }
+    }
+
+    public buttoncheck(): boolean {
+        if() {
+            return true;
+        }
+        return false;
+    }
+
+    */
 }
