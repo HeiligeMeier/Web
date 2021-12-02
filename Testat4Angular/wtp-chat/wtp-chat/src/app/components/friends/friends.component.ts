2400d3aa5eda2d7c664b143a022022dae0204a15
Stefan <steffheili846@gmail.com>
2021-12-02 11:29:49 +0100

    login klappt / friend jetzt nicht so xD

--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
@@ -1,5 +1,6 @@
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Friend } from 'src/app/models/Friend';
 import { User } from 'src/app/models/User';
 import { BackendService } from 'src/app/services/backend.service';
 
@@ -9,8 +10,14 @@ import { BackendService } from 'src/app/services/backend.service';
     styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-    public friends: Array<User> = [];
+    public friends: Friend[] = [];
+    public addList: User[] = [];
     public username: string;
+    public friendrequest: boolean = false;
+    public erg: Boolean = false;
+    public unknownUser: boolean = true;
+    // addNewFriend input value
+    public addNewFriend: string = "";
 
     public constructor(private router: Router, private backend: BackendService) {
         this.username ="";
@@ -21,5 +28,29 @@ export class FriendsComponent implements OnInit {
 
     public loadCurrentUser(): void {
         this.backend.loadCurrentUser();
+    }
+  
+    public userExists(): Boolean {
+        this.backend.userExists(this.addNewFriend)
+        .then((ok: Boolean) => {
+            if (ok) {
+                this.erg = true;
+            }
+            this.erg = false;
+        });
+        return this.erg;
+    }
+    public onSubmit(): void {
+        /*
+        if (this.userExists() == true) {
+            var option = document.createElement('li');
+            let value = "Friend request from " + this.addNewFriend;
+            option.value = value;
+            this.addList.appendchild()
+        } else {
+            this.unknownUser = false;
+        }
+
+        */
     }
 }
--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/login/login.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Message } from 'src/app/models/Message';
 import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
@@ -13,63 +14,29 @@ export class LoginComponent implements OnInit {
     public password: string;
     public usernamevalid: boolean;
     public passwordvalid: boolean;
-    public pwpristine: boolean;
-    public usernamepristine: boolean;
-
-    public constructor(private backend: BackendService, private http: HttpClient, private router: Router) {
+                        // Message?
+    public constructor(private backend: BackendService, private router: Router) {
         this.username = "";
         this.password = "";
-        this.usernamevalid = false;
-        this.passwordvalid = false;
-        this.usernamepristine = true;
-        this.pwpristine = true;
+        this.usernamevalid = true;
+        this.passwordvalid = true;
     }
 
     public ngOnInit(): void {
     }
 
-    
-    public login(): void {
-        if(this.password == '') {
-            this.password = '';
-        }
+    public loginButton(): void {
             this.backend.login(this.username, this.password)
             .then((ok: boolean) => {
                 if(ok) {
                     console.log("Test");
                     this.router.navigate(['/friends']);
                 } else {
-                    this.usernamevalid = false;
                     this.passwordvalid = false;
+                    this.usernamevalid = false;
+                    console.log("Daten nicht korrekt!")
                 }
             });
-    }
-
-
-    public usernamecheck(): void {
-        this.usernamepristine = false;
-        /*  if () {
-            this.usernamevalid = true;
-        }
-        */
-    }
-    
-    public passwordcheck(): void {
-       this.pwpristine = false;
-       /* if () {
-           this.passwordvalid = true;
-       }
-        */
-    }
-
-    public buttoncheck(): boolean {
-        /*
-        if() {
-            return true;
-        }
-        return false;
-    */
-    return true;
     }
 
 }
