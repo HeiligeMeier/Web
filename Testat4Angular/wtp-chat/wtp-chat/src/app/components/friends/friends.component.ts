4d029946938761859be63b27b644b5e7a84a1320
Stefan <steffheili846@gmail.com>
2021-12-06 23:57:37 +0100

    friends nicht fertig

--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ɵɵpureFunction1 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Friend } from 'src/app/models/Friend';
 import { User } from 'src/app/models/User';
 import { BackendService } from 'src/app/services/backend.service';
+import { IntervalService } from 'src/app/services/interval.service';
 
 @Component({
     selector: 'app-friends',
@@ -11,100 +11,139 @@ import { BackendService } from 'src/app/services/backend.service';
     styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-    public friendsArray: Array<Friend> = [];
-    public currentUser: string ="";
-    public friendName: Friend | undefined;
-    public friendrequest: boolean = false;
-    public erg: Boolean = false;
-    public unknownUser: boolean = true;
-    public addNewFriend: string = "";
 
-    public constructor(private http: HttpClient, private router: Router, private backend: BackendService) {
+    // Globale Attribute
+    // friendRequestUser als User? 
+    public friendRequestUser: string = "";
+    public currentUser: string = "";
+    public friendsArray: Array<Friend> = [];
+    public requestList: Array<Friend> = [];
+    public addedDeclined: Boolean = false;
+    public um: Map<String, number> = new Map<string, number>();
+    
+    public TestAnfrage = new Friend("Timo", "", 0);
+    public TestFreund = new Friend("Karen", "accepted", 0);
+    public TestFreund1 = new Friend("Celina", "accepted", 0);
+    public TestFreund2 = new Friend("Hans-Jürgen", "accepted", 0);
+
+    // Konstruktor
+    public constructor(private backend: BackendService, private interval: IntervalService) {
+        // Zur Darstellung
+        this.requestList.push(this.TestAnfrage);
+        this.friendsArray.push(this.TestFreund);
+        this.friendsArray.push(this.TestFreund1);
+        this.friendsArray.push(this.TestFreund2);
     }
 
     public ngOnInit(): void {
         this.loadCurrentUser();
     }
 
+    // loading the logged in User
     public loadCurrentUser(): void {
         let usedUser = this.backend.loadCurrentUser();
         usedUser.then((value: User | null) => {
             if (value != null) {
                 this.currentUser = value.username + "'s Friend";
-            } else{
-                this.currentUser = "Unknown´s Friends";
+            } else {
+                this.currentUser = "Someone's Friends";
             }
             this.getFriendlist();
         });
     }
-  
-    public getFriendlist(): void {     
+
+    // Receive Friendlist from Server
+    public getFriendlist(): void {
         this.backend.loadFriends()
-        .then((value: Array<Friend>) =>
-        {
-          if(value != null) {  
-            console.log(value);
-
-            let karen = new Friend("karen", "accepted", 0);
-            this.friendsArray.push(karen);
-
-
-
-            // Friendlist soll mit Daten befüllt werden
-            for(let i = 0; i < value.length; i++) {                
-                let flist = new Friend(value[i].username, value[i].status, value[i].unreadMessages);
-                this.friendsArray.push(flist);
-            }   
-        //    this.getUnreadMessages();
-        //   console.log(this.um);        
-          }
-
-          else {
-              console.log("User has no friends");
-          }
-        }
-        );        
+            .then((value: Array<Friend>) => {
+                if (value != null) {
+                    console.log(JSON.stringify(value));
+                    for (let i = 0; i < value.length; i++) {
+                        let flist = new Friend(value[i].username, value[i].status, value[i].unreadMessages);
+                        if (flist.status == "accepted") {
+                            this.friendsArray.push(flist);
+                        } else {
+                            this.requestList.push(flist);
+                        }
+                    }
+                    this.getUnreadMessages();
+                }
+            });
     }
 
-    // Friend gibt unreadmessages aus,
-    public friendlist(): void  {
-        this.backend.loadFriends()
-        .then(() => {
-               
-        });
+    // Number of unread Messages
+    public getUnreadMessages() {
+        this.backend.unreadMessageCounts()
+            .then((nameNumber: Map<String, number>) => {
+                if (nameNumber != null) {
+                    this.um = nameNumber;
+                }
+            });
     }
+
+    // addedDeclined wieder auf false setzen wenn anfrage ankommt?
+    // AcceptButtonHandler
+    public acceptFriend() {
+        console.log("Test");
+        this.backend.acceptFriendRequest(this.friendRequestUser)
+            .then((ok: Boolean) => {
+                if (ok) {
+                    let list = new Friend(this.friendRequestUser, "accepted", 0);
+                    this.friendsArray.push(list);
+                    this.addedDeclined = true;
+                    console.log("Accepted Friend!");
+                } else {
+                    // this.addedDeclined = true;
+                    console.log("Friend Accept went wrong!");
+                }
+            });
+    }
+
+    // DismissButtonHandler
+    public declineFriend() {
+        console.log("TestDecline");
+        this.backend.dismissFriendRequest(this.friendRequestUser)
+            .then((ok: Boolean) => {
+                if (ok) {
+                    this.addedDeclined = true;
+                    console.log("Declined Friend!");
+                } else {
+                    // this.addedDeclined = true;
+                    console.log("Friend Decline went wrong!");
+                }
+            });
+    }
+
+    // AddFriendButtonHandler
+    public addFriendButton() {
+        this.backend.userExists(this.friendRequestUser)
+            .then((ok: Boolean) => {
+                if (ok) {
+                    console.log("User exists!");
+                    /*
+                    this.backend.friendRequest(this.friendAdder)
+                        .then((ok: Boolean) => {
+                            if (ok) {
+                                console.log("Added Friend somehow!");
+                            } else {
+                                console.log("Adding Prozess Failed!");
+                            } 
+                        });
+                    */
+                } else {
+                    console.log("User does not exist!");
+                    // alert(this.friendAdder + " does not exist!");
+                }
+            });
+    }
+
+    /*
+    // Intervalservice for continous function use 
     
-    // Buttonhandler von add friendlist
-    public onSubmit(): void {
-        /*
-        if (this.userExists()) {
-        } else {
-            alert("User does not exist!");
-        }
-        */
-    }
-
-    // Überprüft ob Nutzer existiert
-    public userExists(): Boolean {
-        this.backend.userExists(this.addNewFriend)
-        .then((ok: Boolean) => {
-            if (ok) {
-                this.erg = true;
-                this.unknownUser = true;
-            }
-            this.erg = false;
-            this.unknownUser = false;
+    private createInterval(): void {
+        this.interval.setInterval(Component, function(): void => {
+            this.getFriendlist();
         });
-        return this.erg;
     }
-
-    /*  Serveraufrufe
-    loadCurrentUser()
-    userExists(username: string) ?????
-    loadFriends()
-    friendRequest(username: string)
-    acceptFriendRequest(username: string)
-    dismissFriendRequest(username: string)
-    unreadMessageCounts()
     */
 }
