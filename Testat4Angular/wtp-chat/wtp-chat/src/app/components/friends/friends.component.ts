954b2ea4fb80b055d16f4858b456e09384db6ac9
Stefan <steffheili846@gmail.com>
2021-12-06 19:29:03 +0100

    .

--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
@@ -11,71 +11,100 @@ import { BackendService } from 'src/app/services/backend.service';
     styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-    public friends: Friend[] = [];
-    public addList: User[] = [];
+    public friendsArray: Array<Friend> = [];
     public currentUser: string ="";
+    public friendName: Friend | undefined;
     public friendrequest: boolean = false;
     public erg: Boolean = false;
     public unknownUser: boolean = true;
-    // addNewFriend input value
     public addNewFriend: string = "";
-    // unreadmessages?
 
     public constructor(private http: HttpClient, private router: Router, private backend: BackendService) {
     }
 
     public ngOnInit(): void {
-        // lädt aktiven nutzer?
         this.loadCurrentUser();
-        // Freundesliste
-        this.friendlist(this.friends);
     }
 
     public loadCurrentUser(): void {
-        this.backend.loadCurrentUser();
+        let usedUser = this.backend.loadCurrentUser();
+        usedUser.then((value: User | null) => {
+            if (value != null) {
+                this.currentUser = value.username + "'s Friend";
+            } else{
+                this.currentUser = "Unknown´s Friends";
+            }
+            this.getFriendlist();
+        });
     }
   
+    public getFriendlist(): void {     
+        this.backend.loadFriends()
+        .then((value: Array<Friend>) =>
+        {
+          if(value != null) {  
+            console.log(value);
+
+            let karen = new Friend("karen", "accepted", 0);
+            this.friendsArray.push(karen);
+
+
+
+            // Friendlist soll mit Daten befüllt werden
+            for(let i = 0; i < value.length; i++) {                
+                let flist = new Friend(value[i].username, value[i].status, value[i].unreadMessages);
+                this.friendsArray.push(flist);
+            }   
+        //    this.getUnreadMessages();
+        //   console.log(this.um);        
+          }
+
+          else {
+              console.log("User has no friends");
+          }
+        }
+        );        
+    }
+
+    // Friend gibt unreadmessages aus,
+    public friendlist(): void  {
+        this.backend.loadFriends()
+        .then(() => {
+               
+        });
+    }
+    
+    // Buttonhandler von add friendlist
+    public onSubmit(): void {
+        /*
+        if (this.userExists()) {
+        } else {
+            alert("User does not exist!");
+        }
+        */
+    }
+
+    // Überprüft ob Nutzer existiert
     public userExists(): Boolean {
         this.backend.userExists(this.addNewFriend)
         .then((ok: Boolean) => {
             if (ok) {
                 this.erg = true;
+                this.unknownUser = true;
             }
             this.erg = false;
+            this.unknownUser = false;
         });
         return this.erg;
     }
-// Friend gibt unreadmessages aus,
-    public friendlist(getFriends: Friend[]): void  {
-        this.backend.loadFriends()
-        .then(() => {
 
-        });
-    }
-
-    public routing() {
-        let url = "/chat" +
-        this.router.navigate(['/friends']);
-    }
-
-
-
-
-    public onSubmit(): void {
-        /*
-        if (this.userExists() == true) {
-            var option = document.createElement('li');
-            let value = "Friend request from " + this.addNewFriend;
-            option.value = value;
-            this.addList.appendchild()
-        } else {
-            this.unknownUser = false;
-        }
-
-        */
-    }
-
-    public toSettings(){
-        this.router.navigate(['/settings']);
-    }
+    /*  Serveraufrufe
+    loadCurrentUser()
+    userExists(username: string) ?????
+    loadFriends()
+    friendRequest(username: string)
+    acceptFriendRequest(username: string)
+    dismissFriendRequest(username: string)
+    unreadMessageCounts()
+    */
 }
