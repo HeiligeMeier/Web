72207c55f0cf135aa7b9c9b82e4c97ceb0eac6c5
Tergorz <93404085+Tergorz@users.noreply.github.com>
2021-12-08 18:11:01 +0100

    Update start.php

--- Testat5/start.php
+++ Testat5/start.php
@@ -1,1 +1,13 @@
+<?php
 
+define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat');
+define('CHAT_SERVER_ID', '...'); # Ihre Collection ID
+
+
+session_start();
+
+spl_autoload_register(function($class) {
+include str_replace('\\', '/', $class) . '.php';
+});
+
+?>
