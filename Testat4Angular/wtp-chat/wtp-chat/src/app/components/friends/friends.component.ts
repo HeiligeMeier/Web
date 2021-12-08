e8d0d884fea4f53fd3e96eaed607f3a6eb6f2ec3
Stefan <steffheili846@gmail.com>
2021-12-08 15:31:03 +0100

    Testat5 / friends

--- Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
+++ Testat4Angular/wtp-chat/wtp-chat/src/app/components/friends/friends.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Friend } from 'src/app/models/Friend';
 import { User } from 'src/app/models/User';
 import { BackendService } from 'src/app/services/backend.service';
+import { ContextService } from 'src/app/services/context.service';
 import { IntervalService } from 'src/app/services/interval.service';
 
 @Component({
@@ -20,19 +21,22 @@ export class FriendsComponent implements OnInit {
     public requestList: Array<Friend> = [];
     public addedDeclined: Boolean = false;
     public um: Map<String, number> = new Map<string, number>();
-    
+    /*
     public TestAnfrage = new Friend("Timo", "", 0);
     public TestFreund = new Friend("Karen", "accepted", 1);
     public TestFreund1 = new Friend("Celina", "accepted", 4);
     public TestFreund2 = new Friend("Hans-Jürgen", "accepted", 8);
+    */
 
     // Konstruktor
     public constructor(private backend: BackendService, private interval: IntervalService, private context: ContextService) {
         // Zur Darstellung
+        /*
         this.requestList.push(this.TestAnfrage);
         this.friendsArray.push(this.TestFreund);
         this.friendsArray.push(this.TestFreund1);
         this.friendsArray.push(this.TestFreund2);
+        */
     }
 
     public ngOnInit(): void {
@@ -154,4 +158,4 @@ export class FriendsComponent implements OnInit {
         });
     }
     */
-}
+
