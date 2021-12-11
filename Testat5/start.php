822ec0cfe327a53a71d36876366ab2e69bd8bb8a
Stefan <sth5660@thi.de>
2021-12-11 13:48:46 +0100

    ich fange jetzt an

--- Testat5/start.php
+++ Testat5/start.php
@@ -4,7 +4,7 @@ define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat');
 define('CHAT_SERVER_ID', '...'); # Ihre Collection ID
 
 
-session_start();
+// session_start();
 
 spl_autoload_register(function($class) {
 include str_replace('\\', '/', $class) . '.php';
